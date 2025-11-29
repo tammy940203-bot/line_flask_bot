9dd9faba9707c7527c0e3548d00111c65034854c
吳彤恩 <weien171@wutongendeMacBook-Air.local>
2025-11-29 23:11:55 +0800

    fix port binding

--- app.py
+++ app.py
@@ -78,5 +78,7 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
